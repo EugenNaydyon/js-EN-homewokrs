878e110b117b0bdd203007a94dfdb486a7f33d99
EugenNaydyon <eugennaydyon@gmail.com>
2021-05-14 09:01:54 +0300

    include 4 task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-console.log("Hello world!");
+// ======== TASK 1 "объявление пеерменных" ===========
+
+// Change code below this line
+// const productName = "Droid";
+// console.log(productName);
+// // 'Droid'
+// const pricePerItem = 2000;
+// console.log(pricePerItem);
+// 2000
+
+// ===== TASK 2 "Переопределение значения переменной" ===========
+
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// // Change code below this line
+
+// productName = "Repair droid";
+// console.log(productName);
+
+// pricePerItem = 2000 + 1500;
+// console.log(pricePerItem);
+
+// === TASK 3 "Типы примитивов (числа, строки, були)"" ====
+// Change code below this line
+
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+// console.log("topSpeed:", topSpeed);
+// console.log("distance:", distance);
+// console.log("login:", login);
+// console.log("isOnline:", isOnline);
+// console.log("isAdmin:", isAdmin);
+
+// === TASK 4 "математические операторы" =======
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// // Change code below this line
+// const totalPrice = pricePerItem * orderedQuantity;
+
+// console.log("totalPrice:", totalPrice);
+
+// ====== TASK 5 "Шаблонные строки" ======
