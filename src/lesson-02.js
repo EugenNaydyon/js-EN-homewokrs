330b42032cb2f589c596686e1743e93a3cbc4244
EugenNaydyon <eugennaydyon@gmail.com>
2021-05-20 22:59:01 +0300

    ефыл 20

--- src/lesson-02.js
+++ src/lesson-02.js
@@ -140,14 +140,136 @@ function checkStorage(available, ordered) {
 // splitMessage("best_for_week", "_"); // возвращает ["best", "for", "week"]
 
 // ==== TASK 11 Задача: гравировка украшений ====
-function calculateEngravingPrice(message, pricePerWord) {
-  const numbersOfWords = message.split(" ");
-  const totalPrice = numbersOfWords.length * pricePerWord;
-  console.log(numbersOfWords);
-  console.log(totalPrice);
+// function calculateEngravingPrice(message, pricePerWord) {
+//   const numbersOfWords = message.split(" ");
+//   const totalPrice = numbersOfWords.length * pricePerWord;
+//   console.log(numbersOfWords);
+//   console.log(totalPrice);
+
+//   // return totalPrice;
+// }
+
+// calculateEngravingPrice("JavaScript is in my blood", 10); // возвращает 50
+// calculateEngravingPrice("JavaScript is in my blood", 20); // возвращает 100
+// calculateEngravingPrice("Web-development is creative work", 40); // возвращает 160
+// calculateEngravingPrice("Web-development is creative work", 20); // возвращает 80
+
+// ===== TASK 12 Метод массива join() =====
+// function makeStringFromArray(array, delimeter) {
+//   let string;
+
+//   string = array.join(delimeter);
+//   console.log(string);
+
+//   return string;
+// }
+// makeStringFromArray(["Mango", "hurries", "to", "the", "train"], " "); // возвращает "Mango hurries to the train"
+// makeStringFromArray(["M", "a", "n", "g", "o"], " "); // возвращает "Mango"
+// makeStringFromArray(["top", "picks", "for", "you"], "_"); // возвращает "top_picks_for_you"
+
+// ===== TASK 13 Метод массива join() =====
+// function slugify(title) {
+//   // 1. приводим к нижнему регистру
+//   // const titleLowerCase = title.toLowerCase();
+//   // console.log(titleLowerCase);
+//   // // 2. создаем массив из строки
+//   // const titleArray = titleLowerCase.split(" ");
+//   // console.log(titleArray);
+//   // // 3. создаем строку slug с разделителем
+//   // const titleSlug = titleArray.join("-");
+//   // console.log(titleSlug);
+//   // console.log("================================");
+//   const titleModernSlug = title.toLowerCase().split(" ").join("-");
+//   console.log(titleModernSlug);
+// }
+
+// slugify("Arrays for begginers"); // возвращает "arrays-for-begginers"
+// slugify("English for developer"); // возвращает "english-for-developer"
+// slugify("Ten secrets of JavaScript"); // возвращает "ten-secrets-of-javascript"
+// slugify("How to become a JUNIOR developer in TWO WEEKS"); // возвращает "how-to-become-a-junior-developer-in-two-weeks"
+
+// ===== TASK 14 Метод slice() =====
+// const fruits = ["apple", "plum", "pear", "orange", "banana"];
+// console.log(fruits);
+
+// // Change code below this line
+// const firstTwoEls = fruits.slice(0, 2);
+// console.log(firstTwoEls);
+
+// const nonExtremeEls = fruits.slice(1, fruits.length - 1);
+// console.log(nonExtremeEls);
+
+// const lastThreeEls = fruits.slice(-3);
+// console.log(lastThreeEls);
+
+// ===== TASK 15 Метод concat() =====
+// const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// const newClients = ["Peach", "Houston"];
+
+// const allClients = oldClients.concat(newClients); // Change this line
+// console.log(allClients);
+
+// ===== TASK 16 Задача: композиция массивов =====
+// function makeArray(firstArray, secondArray, maxLength) {
+//   let newArray = firstArray.concat(secondArray).slice(0, maxLength);
+
+//   console.log(newArray);
+// }
+
+// makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3); // возвращает ["Mango", "Poly", "Ajax"]
+// makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4); // возвращает ["Mango", "Poly", "Houston", "Ajax"]
+// makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3); // возвращает ["Mango", "Ajax", "Chelsea"]
+// makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2); // возвращает ["Earth", "Jupiter"]
+// makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4); // возвращает ["Earth", "Jupiter", "Neptune", "Uranus"]
+// makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0); // возвращает []
+// // Вызов функции makeArray() со случайными массивами и случайным числом возвращает правильный массив
+
+// ===== TASK 17 Цикл for =====
+// const start = 3;
+// const end = 7;
+
+// for (let i = start; i <= end; i += 1) {
+//   // Change this line
+//   console.log(i);
+// }
+
+// ===== TASK 18 Задача: сумма чисел (цикл for)=====
+// function calculateTotal(number) {
+//   let sum = 0;
+//   for (let i = 0; i <= number; i += 1) {
+//     sum += i;
+//   }
+//   console.log(sum);
+// }
+// calculateTotal(1); // возвращает 1
+// calculateTotal(3); // возвращает 6
+// calculateTotal(7); // возвращает 28
+// calculateTotal(18); // возвращает 171
+// calculateTotal(24); // возвращает 300
+
+// ===== TASK 19 Итерация по массиву =====
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// for (let i = 0; i < fruits.length; i += 1) {
+//   // Change this line
+//   const fruit = fruits[i]; // Change this line
+//   console.log(fruit);
+// }
+
+// ===== TASK 20 Задача: подсчёт суммы покупки =====
+function calculateTotalPrice(order) {
+  let total = 0;
+  let number = order.length - 1;
+  console.log(number);
+
+  for (let i = 0; i < number; i += 1) {
+    total += order[i];
+  }
+
+  return total;
 }
 
-calculateEngravingPrice("JavaScript is in my blood", 10); // возвращает 50
-calculateEngravingPrice("JavaScript is in my blood", 20); // возвращает 100
-calculateEngravingPrice("Web-development is creative work", 40); // возвращает 160
-calculateEngravingPrice("Web-development is creative work", 20); // возвращает 80
+calculateTotalPrice([12, 85, 37, 4]); //возвращает 138
+calculateTotalPrice([164, 48, 291]); //возвращает 503
+calculateTotalPrice([412, 371, 94, 63, 176]); //возвращает 1116
+calculateTotalPrice(); // со случайным массивом возвращает правильное значение
