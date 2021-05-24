7141b84008e1770581742e62188b5b5a29dbb5d6
EugenNaydyon <eugennaydyon@gmail.com>
2021-05-24 09:05:44 +0300

    begin of the new lesson

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// ====== TASK 01 Создание объекта =====
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// console.log(apartment);
+
+// ====== TASK 02 Вложенные свойства =====
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+// console.log(apartment);
+
+// ====== TASK 03 Доступ к свойствам через точку =====
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city centre",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+};
+
+// Change code below this line
+const aptRating = apartment;
+const aptDescr = apartment;
+const aptPrice = apartment;
+const aptTags = apartment;
+// Change code above this line
