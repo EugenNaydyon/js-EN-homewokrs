e807c04fe5e5ab6a9b0d3334bb7d6e2f16e69908
EugenNaydyon <eugennaydyon@gmail.com>
2021-05-24 19:21:28 +0300

    TASK 04 finished

--- src/index.js
+++ src/index.js
@@ -26,17 +26,47 @@
 // console.log(apartment);
 
 // ====== TASK 03 Доступ к свойствам через точку =====
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city centre",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// // Change code below this line
+// const aptRating = apartment.rating;
+// console.log(aptRating);
+
+// const aptDescr = apartment.descr;
+// console.log(aptDescr);
+
+// const aptPrice = apartment.price;
+// console.log(aptPrice);
+
+// const aptTags = apartment.tags;
+// console.log(aptTags);
+// // Change code above this line
+
+// ====== TASK 04 Доступ к вложенным свойствам =====
 const apartment = {
   imgUrl: "https://via.placeholder.com/640x480",
   descr: "Spacious apartment in the city centre",
   rating: 4,
   price: 2153,
   tags: ["premium", "promoted", "top"],
+  owner: {
+    name: "Henry",
+    phone: "982-126-1588",
+    email: "henry.carter@aptmail.com",
+  },
 };
 
 // Change code below this line
-const aptRating = apartment;
-const aptDescr = apartment;
-const aptPrice = apartment;
-const aptTags = apartment;
+const ownerName = apartment;
+const ownerPhone = apartment;
+const ownerEmail = apartment;
+const numberOfTags = apartment;
+const firstTag = apartment;
+const lastTag = apartment;
 // Change code above this line
