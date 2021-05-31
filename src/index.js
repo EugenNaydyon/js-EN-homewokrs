34e62eb6b24018c7d2465108b81d0fc091f82abd
EugenNaydyon <eugennaydyon@gmail.com>
2021-05-31 15:39:52 +0300

    TASK 01

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// =============== TASK 01 ==============
+function makePizza() {
+  return "Ваша пицца готовится, ожидайте.";
+}
+// Пиши код ниже этой строки
+
+const result = makePizza();
+const pointer = makePizza;
+
+console.log(makePizza);
+console.log(makePizza());
