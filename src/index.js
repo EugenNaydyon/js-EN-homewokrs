f00c4c07686f72f585de6322c13870bb2a3bf866
EugenNaydyon <eugennaydyon@gmail.com>
2021-06-07 23:13:01 +0300

    created new branch 05

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// ======== TASK 01 Прототип объекта и метод Object.create() ======
+const parent = {
+  name: "Stacey",
+  surname: "Moore",
+  age: 54,
+  heritage: "Irish",
+};
+// Пиши код ниже этой строки
+
+const child = {};
+
+// Пиши код выше этой строки
+child.name = "Jason";
+child.age = 27;
